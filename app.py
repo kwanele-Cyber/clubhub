d1e229860ae5f3906cbc5de0113e364a0dfaf4e8
Kwanele Zikhali <52756699+kwanele-Cyber@users.noreply.github.com>
2026-03-10 07:03:47 +0200

    feat: auth (database setup + users/index route)

--- app.py
+++ app.py
@@ -1,8 +1,57 @@
+#region imports
+
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
 
-app = Flask('__name__')
+#endregion
 
-#GET /
+
+
+load_dotenv()
+app = Flask(__name__)
+
+
+
+
+#region config
+
+#database Config
+#---------------
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#endregion
+
+
+#region init_extentions
+
+#database setup
+#--------------
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+#endregion
+
+#region models
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+#endregion
+
+#region routes
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -11,6 +60,17 @@ def home():
 def about():
     return render_template('about.html')
 
+#region users_crud
+@app.route('/users')
+def list_users():
+    users = User.query.all()
+    return render_template('users/index.html', users=users)
+
+
+#endregion
+
+
+#endregion
 
 
 if __name__ == '__main__':
